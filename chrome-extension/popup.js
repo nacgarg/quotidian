57820d199d404d10806314b0117ad930c8a786fb
nacgarg <nachiketa.gargi@gmail.com>
2014-12-30 12:08:04 -0800

    new ui and tts

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,10 +2,16 @@ var xmlhttp = new XMLHttpRequest();
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
 
 function display(object) {
-    document.getElementById('quote').innerHTML = object.quoteText;
+    document.getElementById('quote').innerHTML = object.quoteText+'<a><span id="sound" class="glyphicon glyphicon-volume-up"></span></a>';
     document.getElementById('author').innerHTML = object.quoteAuthor;
     document.getElementById('author').addEventListener("click", function() {
-        window.open(object.quoteLink)
+        window.open('https://en.wikipedia.org/wiki/Special:Search/' + object.quoteAuthor);
+    });
+    document.getElementById('sound').addEventListener("click", function() {
+        chrome.tts.speak(object.quoteText);
+    });
+    document.getElementById('asdf').addEventListener("click", function() {
+        window.open('http://www.forismatic.com/en')
     });
 }
 xmlhttp.open("POST", url, true);
