307d87b50ad6ffdce3fdf009bffd5f7446c3bb3d
nacgarg <nachiketa.gargi@gmail.com>
2014-12-30 20:46:50 -0800

    next quote button

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
 
 function display(object) {
-    document.getElementById('quote').innerHTML = '"'+object.quoteText+'" <a><span id="sound" class="glyphicon glyphicon-volume-up"></span></a>';
+    document.getElementById('quote').innerHTML = object.quoteText + ' <a><span id="sound" class="glyphicon glyphicon-volume-up"></span></a>';
     document.getElementById('author').innerHTML = object.quoteAuthor;
     document.getElementById('author').addEventListener("click", function() {
         window.open('https://en.wikipedia.org/wiki/Special:Search/' + object.quoteAuthor);
@@ -13,10 +13,10 @@ function display(object) {
     document.getElementById('asdf').addEventListener("click", function() {
         window.open('http://www.forismatic.com/en')
     });
-    document.getElementById('secret').value = '"'+object.quoteText+'" - '+ object.quoteAuthor+'\r\nVia Quotidian - http://ngargi.me/quotidian';
+    document.getElementById('secret').value = '"' + object.quoteText + '" - ' + object.quoteAuthor + '\r\nVia Quotidian - http://ngargi.me/quotidian';
     document.getElementById('asdfg').addEventListener("click", function() {
-    	document.getElementById('secret').select();
-    	document.execCommand('copy', true);
+        document.getElementById('secret').select();
+        document.execCommand('copy', true);
     });
 }
 xmlhttp.open("POST", url, true);
@@ -30,3 +30,16 @@ xmlhttp.onreadystatechange = function() {
 }
 
 xmlhttp.send();
+document.getElementById('next').addEventListener("click", function() {
+    xmlhttp.open("POST", url, true);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var myArr = JSON.parse(xmlhttp.responseText);
+            display(myArr);
+
+
+        }
+    }
+
+    xmlhttp.send();
+});
